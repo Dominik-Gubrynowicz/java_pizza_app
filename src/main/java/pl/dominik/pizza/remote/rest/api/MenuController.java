ca2a2948aefed18de8c5f07fd4bafa32dca92975
Dominik Gubrynowicz <dominik.gubrynowicz@gmail.com>
2021-11-16 13:41:12 +0100

    Added menu mapping

--- src/main/java/pl/dominik/pizza/remote/rest/api/MenuController.java
+++ src/main/java/pl/dominik/pizza/remote/rest/api/MenuController.java
@@ -2,6 +2,7 @@ package pl.dominik.pizza.remote.rest.api;
 
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.dominik.pizza.domain.model.SizeType;
@@ -18,6 +19,7 @@ import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 @RestController
 public class MenuController  {
 
+    @GetMapping
     public ResponseEntity<MenuDto> getMenu() {
         SizeDto sizeDto = new SizeDto(1, SizeType.S, BigDecimal.valueOf(30.00));
         PizzaDto pizzaDto = new PizzaDto(1, "Margherita", List.of(sizeDto));
